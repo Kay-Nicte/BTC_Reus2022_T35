61f800691603cfb927c1be1c5432d2158e9c1c66
Ixabel Justo <miju355@vidalibarraquer.net>
2022-06-09 09:42:04 +0200

    Ejercicio 35_2

--- Problema2/src/app/app.component.ts
+++ Problema2/src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Problema2';
+  nombre: string = '';
+  email = '';
+  mensaje = '';
+  calculo = '';
+
+  validarFormulario(): void {
+    // let inputNombre: HTMLInputElement = document.querySelector('#nombre');
+
+    // Variables
+    let errores: string[] = [];
+
+    // Validar el nombre (por si es unu número)
+    if (isNaN(parseInt(this.nombre)))
+      errores.push('El nombre no puede ser un número');
+
+    // El nombre es obligatorio
+    if (this.nombre === '') errores.push('El nombre es obligatorio');
+  }
 }
